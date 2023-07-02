c87da934a7a937e5c29ad01d87f2c0095df7c502
ankitkumar <lostankit7@gmail.com>
2023-07-02 12:28:19 +0530

    updated code

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import "../styles/globals.css"
-import Link from "next/Link"
+import "../styles/globals.css";
+import Link from "next/link";
 
-function Marketplace({Component,pageProps}){
-  return(
+function Marketplace({ Component, pageProps }) {
+  return (
     <div className="border-b p-6 bg-gradient-to-b from-purple-600 to-blue-600">
-      <nav className="border-b p-6">
+      <nav className="border-b p-6 ">
         <p className="text-4xl font-bold flex justify-center text-cyan-200">
           NFT Digital Art Marketplace
         </p>
@@ -13,7 +13,7 @@ function Marketplace({Component,pageProps}){
             Home
           </Link>
           <Link href="/createNFT" className="mr-6 text-xl text-cyan-200">
-            Sell Your NFTs
+            Sell NFTs
           </Link>
           <Link href="/myNFTs" className="mr-6 text-xl text-cyan-200">
             My NFTs
@@ -23,8 +23,9 @@ function Marketplace({Component,pageProps}){
           </Link>
         </div>
       </nav>
-      <Component{...pageProps}/>
+      <Component {...pageProps} />
     </div>
-  )
+  );
 }
-export default Marketplace
+
+export default Marketplace;
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,51 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
-import axios from "axios";
-import web3Modal from "web3modal";
-import { nftAddress, nftMarketAddress } from "../config";
+import axios, { HttpStatusCode } from "axios";
+import Web3Modal from "web3modal";
+import { nftaddress, nftmarketaddress } from "../config";
+
+//const ipfsClient = require('ipfs-http-client');
+
 import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
 import NFTMarket from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 
 export default function Home() {
-  const [nfts, setNfts] = useState([]);
+  const [nfts, setNFts] = useState([]);
   const [loadingState, setLoadingState] = useState("not-loaded");
 
   useEffect(() => {
-    loadNfts();
+    loadNFTs();
   }, []);
 
-  async function loadNfts() {
+  async function loadNFTs() {
     const provider = new ethers.providers.JsonRpcProvider();
-    const nftContract = new ethers.Contract(nftAddress, NFT.abi, provider);
+    const nftContract = new ethers.Contract(nftaddress, NFT.abi, provider);
     const nftMarketContract = new ethers.Contract(
-      nftMarketAddress,
+      nftmarketaddress,
       NFTMarket.abi,
       provider
     );
-    const data = await nftMarketContract.fetchMarketItems()
+    const data = await nftMarketContract.fetchMarketItems();
 
     const items = await Promise.all(
-      data.map(async (it) => {
-        const tokenUri = await nftContract.tokenURI(it.tokenId);
-        const meta = axios.get(tokenUri);
-        let price = ethers.utils.formatUnits(it.price.toString, "ether");
-
+      data.map(async (i) => {
+        const tokenUri = await nftContract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
         let item = {
-          price: price,
-          tokenId: it.tokenId.toNumber(),
-          seller: it.seller,
-          owner: it.owner,
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
           image: meta.data.image,
-          name: meta.data.image,
+          name: meta.data.name,
           description: meta.data.description,
         };
         console.log(meta);
         return item;
       })
     );
-
-    setNfts(items);
+    setNFts(items);
     setLoadingState("loaded");
   }
 
@@ -53,49 +54,45 @@ export default function Home() {
     const connection = await web3Modal.connect();
     const provider = new ethers.providers.Web3Provider(connection);
 
-    const signer = await provider.getSigner();
+    const signer = provider.getSigner();
     const contract = new ethers.Contract(
-      nftMarketAddress,
+      nftmarketaddress,
       NFTMarket.abi,
       signer
     );
     const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
     const transaction = await contract.createMarketSale(
-      nftAddress,
+      nftaddress,
       nft.tokenId,
-      { value: price }
+      {
+        value: price,
+      }
     );
-    await transaction.await();
-    loadNfts();
+    await transaction.wait();
+    loadNFTs();
   }
 
-  if (loadingState === "loaded" && !nfts.length) {
+  if (loadingState === "loaded" && !nfts.length)
     return (
       <div>
-        <p className="px-10 py-10 text-2xl font-bold justify-center text-cyan-200">
+        <p className="px-10 py-10 text-2xl font-bold flex justify-center text-cyan-200">
           There are currently no NFTs in the Marketplace.
           <br /> Please come back later
         </p>
       </div>
     );
-  }
 
   return (
     <div className="flex justify-center">
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-4 gap-5 pt-5">
-          {nfts.map((nft, index) => {
-            // todo with (
-            <div
-              key={index}
-              className="border shadow rounded-3xl overflow-hidden"
-            >
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 pt-5">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-3xl overflow-hidden">
               <img src={nft.image} />
               <div className="p-4">
                 <p
                   style={{ height: "64px" }}
-                  className="text-2xl"
-                  font-semibold
+                  className="text-2xl font-semibold"
                 >
                   {nft.name}
                 </p>
@@ -115,10 +112,10 @@ export default function Home() {
                   Buy
                 </button>
               </div>
-            </div>;
-          })}
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
